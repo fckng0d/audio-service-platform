c6d2ab97dda8d7cfb3be54acccaffa916e21a78b
fckng0d <soulless_august@mail.ru>
2025-05-04 02:16:46 +0300

    feat(infra): add config-server and api-gateway
    build(docker): configure containers via docker-compose

---
+++ infrastructure/config-server/src/test/java/com/github/fckng0d/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fckng0d.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
